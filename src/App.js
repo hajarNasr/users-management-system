e16d0d449bbed9427d28eeeb2ce2335b22b3f5ef
hajarNasr <hajar@weventure.global>
2021-07-26 19:58:03 +0200

    style: add a fixed button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
 import Header from "./components/Header";
+import AddUserBtn from "./components/AddUserBtn";
+
 import "./App.css";
 
 function App() {
+  const [isShowForm, setIsShowForm] = useState(false);
+
+  const showForm = () => setIsShowForm(true);
+
   return (
     <div className="App">
       <Header title="Users" />
+      <AddUserBtn onClick={showForm} />
     </div>
   );
 }
---
+++ src/components/AddUserBtn.js
@@ -0,0 +1,7 @@
+const AddUserBtn = ({ onClick }) => (
+  <button onClick={onClick} className="add-user-btn">
+    Add User
+  </button>
+);
+
+export default AddUserBtn;
