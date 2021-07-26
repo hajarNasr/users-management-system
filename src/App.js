b6f2e4b7ba6e333a616e91ebe19f581eec09c2a6
hajarNasr <hajar@weventure.global>
2021-07-26 19:48:18 +0200

    style: add a fixed header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header title="Users" />
+    </div>
+  );
 }
 
 export default App;
